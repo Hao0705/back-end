ff797f7a01c415d65f1b1336400f30e11795d3af
Pham Cong Hao 20215045 <hao.pc215045@sis.hust.edu.vn>
2026-07-26 20:02:37 +0700

    refactor: replace createConnection with createPool

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express();
 const port = process.env.PORT;
@@ -14,19 +14,11 @@ configViewEngine(app);
 app.use(webRoutes);
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    user: "root",
-    password: "123456",
-    database: 'hoidanit'
-});
 // simple query 
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
         console.log(">>>> Results=", results);
-        console.log(">>>> Fields=", fields);
     }
 )
 app.listen(port, () => {
